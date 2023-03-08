c5542f03b5f08d796b6ca539da44bc9d94c824cc
DavidPrado25 <davidprado.j25@gmail.com>
2023-03-08 15:01:10 -0400

    cmb-animaciones-8/03/2023

---
+++ js/animaciones.js
@@ -0,0 +1,21 @@
+window.sr=ScrollReveal();
+ 
+//Efecto para logo.
+sr.reveal('.img-responsive2',{
+    duration:9000,
+    rotate:{
+      x:2,
+      y:180, 
+    }
+});
+
+sr.reveal('.cintilloLogoGobierno',{
+  duration:1500
+});
+
+ScrollReveal().reveal('.navbar-collapse', { duration: 2700 });
+ScrollReveal().reveal('.carousel', { duration: 4000 });
+
+ScrollReveal().reveal('.productoImagen', {
+  duration: 4000
+});
