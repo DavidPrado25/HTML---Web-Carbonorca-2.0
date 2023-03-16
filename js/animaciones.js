1e9b65b2819314658a03a997f0ef94a7d075aca8
DavidPrado25 <davidprado.j25@gmail.com>
2023-03-16 12:32:32 -0400

    cmb-animaciones-16/03/2023

--- js/animaciones.js
+++ js/animaciones.js
@@ -2,11 +2,20 @@ window.sr=ScrollReveal();
  
 //Efecto para logo.
 sr.reveal('.img-responsive2',{
-    duration:9000,
+    duration:7000,
     rotate:{
       x:2,
       y:180, 
     }
+});
+
+ScrollReveal().reveal('.nav-buttons',{ 
+  rotate:{
+    x:120,
+    y:0,
+    z:2 
+  } 
+
 });
 
 sr.reveal('.cintilloLogoGobierno',{
@@ -17,14 +26,44 @@ sr.reveal('.cintilloLogoGobierno',{
     z:2 
   }
 });
+ScrollReveal().reveal('.navbar-collapse', { duration: 2000 });
+ScrollReveal().reveal('.container-fluid', { duration: 2000 });
+ScrollReveal().reveal('.carousel', { duration: 3000 });
 
-ScrollReveal().reveal('.navbar-collapse', { duration: 2700 });
-ScrollReveal().reveal('.carousel', { duration: 4000 });
-
-ScrollReveal().reveal('.productoImagen', {
+ScrollReveal().reveal('.wrapper', { 
   origin   : 'bottom',
   distance : '20px',
-  duration : 2000,
+  duration : 3500,
+  easing   : 'cubic-bezier(0.5, 0, 0, 1)',
+  interval : 200 
+
+});
+
+ScrollReveal().reveal('.container-fluid', {
+  origin   : 'bottom',
+  distance : '20px',
+  duration : 5000,
+  easing   : 'cubic-bezier(0.5, 0, 0, 1)',
+  interval : 200 
+})
+
+ScrollReveal().reveal('.col-sm-3', {
+  rotate:{
+    x:120,
+    y:0,
+    z:2 
+  },
+  origin   : 'bottom',
+  distance : '20px',
+  duration : 3000,
+  easing   : 'cubic-bezier(0.5, 0, 0, 1)',
+  interval : 200
+});
+
+ScrollReveal().reveal('.footer', {
+  origin   : 'bottom',
+  distance : '20px',
+  duration : 4000,
   easing   : 'cubic-bezier(0.5, 0, 0, 1)',
   interval : 200
 });
